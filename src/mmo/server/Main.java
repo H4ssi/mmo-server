1aa5cef4e6f3ba2751f228e6e0b03b6c9496e2af
Florian Hassanen <florian.hassanen@gmail.com>
2015-05-31 17:44:35 +0200

    provide mechanism to shutdown server

--- src/mmo/server/Main.java
+++ src/mmo/server/Main.java
@@ -2,10 +2,17 @@ package mmo.server;
 
 import dagger.ObjectGraph;
 
+import java.io.IOException;
+
 public class Main {
-	public static void main(String[] args) {
-		ObjectGraph objectGraph = ObjectGraph.create(new ServerModule());
-		Server server = objectGraph.get(Server.class);
-		server.run();
-	}
+    public static void main(String[] args) throws IOException,
+            InterruptedException {
+        ObjectGraph objectGraph = ObjectGraph.create(new ServerModule());
+        Server server = objectGraph.get(Server.class);
+        server.run();
+        System.out.println("Server started (press enter to shutdown)");
+        System.in.read();
+        System.out.println("Server shutting down...");
+        server.shutdown();
+    }
 }
--- src/mmo/server/Server.java
+++ src/mmo/server/Server.java
@@ -7,31 +7,49 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.util.HashedWheelTimer;
+import io.netty.util.concurrent.Future;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
 
 public class Server {
     private final Provider<Handler> handlerProvider;
+    private final HashedWheelTimer timer;
+    private NioEventLoopGroup parentGroup;
+    private NioEventLoopGroup childGroup;
+
 
     @Inject
-    public Server(Provider<Handler> handlerProvider) {
+    public Server(Provider<Handler> handlerProvider, HashedWheelTimer timer) {
         this.handlerProvider = handlerProvider;
+        this.timer = timer;
     }
 
     public void run() {
+        parentGroup = new NioEventLoopGroup();
+        childGroup = new NioEventLoopGroup();
         new ServerBootstrap()
-                .group(new NioEventLoopGroup(), new NioEventLoopGroup())
+                .group(parentGroup, childGroup)
                 .channel(NioServerSocketChannel.class)
                 .childHandler(new ChannelInitializer<SocketChannel>() {
                     protected void initChannel(SocketChannel ch)
                             throws Exception {
-                        ch.pipeline().addLast(new HttpServerCodec())
+                        ch.pipeline()
+                                .addLast(new HttpServerCodec())
                                 .addLast(handlerProvider.get());
                     }
+                })
+                .option(ChannelOption.TCP_NODELAY, true)
+                .childOption(ChannelOption.TCP_NODELAY, true)
+                .bind(8080);
+    }
 
-                    ;
-                }).option(ChannelOption.TCP_NODELAY, true)
-                .childOption(ChannelOption.TCP_NODELAY, true).bind(8080);
+    public void shutdown() throws InterruptedException {
+        timer.stop();
+        Future<?> parentShutdown = parentGroup.shutdownGracefully();
+        Future<?> childShutdown = childGroup.shutdownGracefully();
+        parentShutdown.await();
+        childShutdown.await();
     }
 }
