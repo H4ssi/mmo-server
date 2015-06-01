198799e4f4c85175a2b6ad23ee21f3b174bdf3ee
Florian Hassanen <florian.hassanen@gmail.com>
2015-06-01 19:34:24 +0200

    license code under AGPL

--- src/mmo/server/NotificationHandler.java
+++ src/mmo/server/NotificationHandler.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2015 Florian Hassanen
+ *
+ * This file is part of mmo-server.
+ *
+ * mmo-server is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * mmo-server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with mmo-server.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 package mmo.server;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -116,9 +136,6 @@ public class NotificationHandler extends ChannelInboundHandlerAdapter {
         String[] msgs = new String[]{
                 "<big>Welcome to POS1-mmo!</big>",
                 "<big><strong>Please use responsibly!</strong></big>",
-                "<big><strong>" +
-                        "(e.g. do not use it for cheating at tests!!!)" +
-                        "</strong></big>",
 
                 "Find code and updates here: " +
                         "<ul>" +
@@ -133,6 +150,12 @@ public class NotificationHandler extends ChannelInboundHandlerAdapter {
                         "</a>" +
                         "</li>" +
                         "</ul>",
+
+                "<small>mmo-server is free software, and you are welcome to " +
+                        "redistribute it under the terms of the GNU AGPL: " +
+                        "<a href='https://gnu.org/licenses/agpl.html'>" +
+                        "https://gnu.org/licenses/agpl.html" +
+                        "</a></small>",
 
                 "<small>This server should receive non-breaking " +
                         "updates only!<br>" +
--- src/mmo/server/Room.java
+++ src/mmo/server/Room.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2015 Florian Hassanen
+ *
+ * This file is part of mmo-server.
+ *
+ * mmo-server is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * mmo-server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with mmo-server.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 package mmo.server;
 
 import com.google.common.collect.BiMap;
@@ -7,9 +27,6 @@ import mmo.server.model.Coord;
 import java.util.Collections;
 import java.util.Map;
 
-/**
- * Created by florian on 5/31/15.
- */
 public class Room {
     private final static int SIZE = 16;
 
